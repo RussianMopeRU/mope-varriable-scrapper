06cb0f96f89f5fc7f1064295eaca945276f76874
RussianMope <74246343+RussianMopeRU@users.noreply.github.com>
2023-04-30 14:51:33 +0300

    Create MopeRegexp.js

---
+++ MopeRegexp.js
@@ -0,0 +1,12 @@
+class MopeRegexp {
+    constructor(script) {
+        this.script = script;
+    }
+
+    get(regexp) {
+        let result = this.script.match(regexp)
+        return result || new Error(`Wrong regexp: ${regexp}`);
+    }
+}
+
+module.exports = {};
