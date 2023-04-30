1de483f4739d10db89d89aba7946cdcef466a168
RussianMope <74246343+RussianMopeRU@users.noreply.github.com>
2023-04-30 14:19:23 +0300

    Setup basic stuff

---
+++ scrapper.js
@@ -0,0 +1,11 @@
+const axios = require(`axios`).default;
+
+// Add a standart user agent to prevent cloudflare blocking our requests.
+axios.interceptors.request.use((config) => {
+    config.headers[`User-Agent`] = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.64`;
+    return config;
+});
+
+
+
+module.exports = {};
