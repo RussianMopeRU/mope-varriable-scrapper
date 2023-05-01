9359ad5fc2cf29955ae6a69423ddc0f4425e7ab2
RussianMope <74246343+RussianMopeRU@users.noreply.github.com>
2023-05-01 19:23:45 +0300

    Add gameMouseX, gameMouseY, camzoom, camx and camy

--- filter.js
+++ filter.js
@@ -10,24 +10,51 @@ let filterMopeScript = (script) => {
         richPresences.push(obj.slice(`:`.length).slice(0, -",".length));
     })
 
-    let gameObjsByIDs = new MopeRegexp(richPresences[5]).get(/_(.*?)\[/g)[0].slice(0, -`]`.length);
+    let gameObjsByID = new MopeRegexp(richPresences[5]).get(/_(.*?)\[/g)[0].slice(0, -`]`.length);
     let gameObjs = new MopeRegexp(new MopeRegexp(script).get(/]\)\)delete _0(.*?)\);}/g)[0]).get(/=_0x(.*?)\['/g)[0].slice("=".length).slice(0, -"['".length);
 
     let MsgWriter = new MopeRegexp(script).get(/newMsg=(.*?)\),/g)[0].slice(`newMsg=new `.length).slice(0, -`(0x2),`.length)
     let wsSendMsg = new MopeRegexp(new MopeRegexp(script).get(/newMsg=new(.*?)\(new/g)[0]).get(/\),_0(.*?)\(/g)[0].slice(`),`.length).slice(0, -`(`.length);
 
+    let gameMouse = new MopeRegexp(new MopeRegexp(new MopeRegexp(script).get(/']&&document\['(.*?),mes/g)[0]).get(/\|\|Math\['(.*?),m/g)[0]).get(/\){(.*?),m/g)[0].slice(`){`.length).slice(0, -`,m`.length)
+    let gameMouseX = new MopeRegexp(gameMouse).get(/=(.*?),/g)[0].slice(`=`.length).slice(0, -`,`.length);
+    let gameMouseY = new MopeRegexp(gameMouse).get(/(?<==)_0x[0-9a-f]{6}(?!\s*,)/g)[0];
+
+    let camzoom = new MopeRegexp(new MopeRegexp(script).get(/\*_0x[0-9a-f]{6}\)\)\/_0x[0-9a-f]{6};/)[0]).get(/\/(.*?);/)[0].slice("/".length).slice(0, -";".length);
+    let cam = new MopeRegexp(script).get(/camzoom_n(.*?)=!\[]/)[0];
+    let camx = new MopeRegexp(cam).get(/0x1,(.*?),_/)[1].slice(0, -"=0x0".length);
+    let camy = new MopeRegexp(cam).get(/=0x0,(.*?)=/)[1];
+
+    let pvpArenaObj = new MopeRegexp(new MopeRegexp(new MopeRegexp(script).get(/:default:return(.*?)=null/)[0]).get(/!\[](.*?)=null/)[1].slice(`,`.length)).get(/(?<=,)_0x([^\W_]+)(?=,|$)/)[0]; 
+    
+    console.log(pvpArenaObj)
+    
     let allVarriables = {
         ani: richPresences[0],
         species: richPresences[1],
         subspecies: richPresences[2],
         xp: richPresences[3],
+        
         server: richPresences[4],
+        
         player: richPresences[5],
-        playerID: richPresences[6],
-        gameObjsByIDs: gameObjsByIDs,
+        selfId: richPresences[6],
+
+        gameObjsByID: gameObjsByID,
         gameObjs: gameObjs,
+
         MsgWriter: MsgWriter,
-        wsSendMsg: wsSendMsg
+        wsSendMsg: wsSendMsg,
+
+        gameMouseX: gameMouseX,
+        gameMouseY: gameMouseY,
+
+        camzoom: camzoom,
+        camx: camx,
+        camy: camy,
+
+        pvpArenaObj: pvpArenaObj,
+        
     };
 
     console.log(allVarriables)
